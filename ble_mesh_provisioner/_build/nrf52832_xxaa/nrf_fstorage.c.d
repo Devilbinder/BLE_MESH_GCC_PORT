2568b97dd7e94ff9840732a8ce5c3a70e6894272
Devilbinder <Devilbinder@users.noreply.github.com>
2018-07-12 12:56:36 +0200

    remove build files

--- ble_mesh_provisioner/_build/nrf52832_xxaa/nrf_fstorage.c.d
+++
@@ -1,7 +0,0 @@
-_build/nrf52832_xxaa/nrf_fstorage.c.o: \
- C:/nRF5_SDK_15/nRF5_SDK_15.0.0_a53641a/components/libraries/fstorage/nrf_fstorage.c \
- sdk_config.h app_config.h
-
-sdk_config.h:
-
-app_config.h:
--- ble_mesh_server/_build/nrf52832_xxaa/SEGGER_RTT_Syscalls_GCC.c.d
+++
@@ -1,7 +0,0 @@
-_build/nrf52832_xxaa/SEGGER_RTT_Syscalls_GCC.c.o: \
- C:/nRF5_SDK_15/nRF5_SDK_15.0.0_a53641a/external/segger_rtt/SEGGER_RTT_Syscalls_GCC.c \
- sdk_config.h app_config.h
-
-sdk_config.h:
-
-app_config.h:
